aa66b0f2f4c4fd0021c924707ffefd181f26cc79
yeseul <yesul0718@naver.com>
2025-05-28 06:22:46 +0900

    fin WebSocketConfig.java

--- backend/src/main/java/org/example/speaknotebackend/config/WebSocketConfig.java
+++ backend/src/main/java/org/example/speaknotebackend/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package org.example.speaknotebackend.config;
 
-import org.example.speaknotebackend.websocket.AnnotationWebSocketHandler;
 import org.example.speaknotebackend.websocket.AudioWebSocketHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,23 +9,18 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final AnnotationWebSocketHandler annotationWebSocketHandler;
     private final AudioWebSocketHandler audioWebSocketHandler;
 
     @Value("${custom.cors.allowed-origin}")
     private String allowedOrigin;
 
     public WebSocketConfig(
-            AnnotationWebSocketHandler annotationWebSocketHandler,
             AudioWebSocketHandler audioWebSocketHandler) {
-        this.annotationWebSocketHandler = annotationWebSocketHandler;
         this.audioWebSocketHandler = audioWebSocketHandler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(annotationWebSocketHandler, "/ws/annotation")
-                .setAllowedOrigins(allowedOrigin);
 
         registry.addHandler(audioWebSocketHandler, "/ws/audio")
                 .setAllowedOrigins(allowedOrigin);
